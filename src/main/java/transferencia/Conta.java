2e70bc537fb688658ba720ef5f8ef757c198eaa4
Danilo Gripa <danilogripa@gmail.com>
2021-11-16 21:42:07 -0300

    First Commit 2

--- src/main/java/transferencia/Conta.java
+++ src/main/java/transferencia/Conta.java
@@ -29,4 +29,25 @@ public class Conta {
     public Cliente getProprietario() {
         return proprietario;
     }
+
+    public void realizarDeposito(double valor){
+        saldo = saldo + valor;
+    }
+
+    public boolean realizarSaque(double valor){
+        if(valor > saldo){
+            return false;
+        }
+        saldo -= valor;
+        return true;
+    }
+
+    public boolean realizarTransferencia (double valor, Conta destino){
+        if(realizarSaque(valor)){
+            destino.realizarDeposito(valor);
+            return true;
+        }
+    }
+
+
 }
---
+++ src/test/java/transferencia/ContaTest.java
@@ -0,0 +1,36 @@
+package transferencia;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContaTest {
+
+    @Test
+    void getAgencia() {
+    }
+
+    @Test
+    void getNumeroConta() {
+    }
+
+    @Test
+    void getSaldo() {
+    }
+
+    @Test
+    void getProprietario() {
+    }
+
+    @Test
+    void realizarDeposito() {
+    }
+
+    @Test
+    void realizarSaque() {
+    }
+
+    @Test
+    void realizarTransferencia() {
+    }
+}
